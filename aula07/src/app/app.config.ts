9af25d8b277b049a66e2010aaf8978e703c9c1ef
rafael1985matos-droide <rafael1985matos@gmail.com>
2026-06-03 22:22:43 +0100

    Firebase

--- aula07/src/app/app.config.ts
+++ aula07/src/app/app.config.ts
@@ -2,6 +2,9 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp } from 'firebase/app';
+
+export const app = initializeApp(environment.firebaseConfig);
 
 export const appConfig: ApplicationConfig = {
   providers: [
---
+++ aula07/src/environment/environment.prod.ts
@@ -0,0 +1,13 @@
+import { keys } from './environment.keys';
+
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: keys.apiKey,
+    authDomain: 'frontend-cinel-rafa-efe33.firebaseapp.com',
+    projectId: 'frontend-cinel-rafa-efe33',
+    storageBucket: 'frontend-cinel-rafa-efe33.firebasestorage.app',
+    messagingSenderId: '210106175239',
+    appId: '1:210106175239:web:dfa48aa2dce7cb7a69bfa2',
+  },
+};
