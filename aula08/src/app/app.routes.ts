32307e2ae45fe32906a92f71ce8760b7032e12df
rafael1985matos-droide <rafael1985matos@gmail.com>
2026-06-09 21:31:44 +0100

    2 commit

--- aula08/src/app/app.routes.ts
+++ aula08/src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
+import { Lacos } from './pages/lacos/lacos';
 
 export const routes: Routes = [
-    {path: "", redirectTo: "welcome", pathMatch: "full"},
-    {},
+    {path: "", redirectTo: "ebook/welcome", pathMatch: "full"},
+    {path: "ebook/welcome", loadComponent: () => import("./pages/welcome/welcome").then(ana => ana.Welcome)},
+    {path: "ebook/lacos", component: Lacos},
+    {path: "ebook/tipo-de-dados", loadComponent: () => import("./pages/tipo-de-dados/tipo-de-dados").then(paulo => paulo.TipoDeDados)},
+    {path: "ebook/variaveis", loadComponent: () => import("./pages/variaveis/variaveis").then(leo => leo.Variaveis)},
+    {path: "ebook/operadores", loadComponent: () => import("./pages/operadores/operadores").then(c => c.Operadores)},
+    {path: "ebook/estrutura-de-controles", loadComponent: () => import("./pages/estrutura-de-controles/estrutura-de-controles").then(rafael => rafael.EstruturaDeControles)},
+
 
 
     /**ultima rota */
-    {path: "**" },
+    {path: "**", loadComponent: () => import("./pages/welcome/welcome").then(c => c.Welcome)} ,
 ];
