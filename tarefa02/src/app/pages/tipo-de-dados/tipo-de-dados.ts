74e016cb49bec8e8323bbffa873d627d5303a058
rafael1985matos-droide <rafael1985matos@gmail.com>
2026-06-21 23:09:55 +0100

    rotas a funcionar

--- tarefa02/src/app/pages/tipo-de-dados/tipo-de-dados.ts
+++ tarefa02/src/app/pages/tipo-de-dados/tipo-de-dados.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tipo-de-dados',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tipo-de-dados.html',
   styleUrl: './tipo-de-dados.css',
 })
