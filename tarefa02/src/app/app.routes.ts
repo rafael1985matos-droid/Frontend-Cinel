04bf88850ca5eaeb5da86a2b06ca12357c0f41aa
rafael1985matos-droide <rafael1985matos@gmail.com>
2026-06-21 23:45:26 +0100

    pages commit

--- tarefa02/src/app/app.routes.ts
+++ tarefa02/src/app/app.routes.ts
@@ -2,19 +2,40 @@ import { Routes } from '@angular/router';
 import { Lacos } from './pages/lacos/lacos';
 
 export const routes: Routes = [
-   {path: "", redirectTo: "ebook/welcome", pathMatch: "full"},
-   {path: "ebook/welcome", loadComponent: () => import("./pages/welcome/welcome").then(rafael => rafael.Welcome)},
-   {path: "ebook/lacos", component: Lacos},
-   {path: "ebook/tipo-de-dados", loadComponent: () => import("./pages/tipo-de-dados/tipo-de-dados").then(paulo => paulo.TipoDeDados)},
-   {path: "ebook/variaveis", loadComponent: () => import("./pages/variaveis/variaveis").then(leo => leo.Variaveis)},   
-   {path: "ebook/operadores", loadComponent: () => import("./pages/operadores/operadores").then(c => c.Operadores)},
-   {path: "ebook/estrututa-de-controles", loadComponent: () => import("./pages/estrutura-de-controles/estrutura-de-controles").then(ana => ana.EstruturaDeControles)},
+  {
+    path: '',
+    redirectTo: 'ebook/welcome',
+    pathMatch: 'full',
+  },
+  {
+    path: 'ebook/welcome',
+    loadComponent: () => import('./pages/welcome/welcome').then((rafael) => rafael.Welcome),
+  },
+  {
+    path: 'ebook/lacos',
+    component: Lacos,
+  },
+  {
+    path: 'ebook/tipo-de-dados',
+    loadComponent: () =>
+      import('./pages/tipo-de-dados/tipo-de-dados').then((paulo) => paulo.TipoDeDados),
+  },
+  {
+    path: 'ebook/variaveis',
+    loadComponent: () => import('./pages/variaveis/variaveis').then((leo) => leo.Variaveis),
+  },
+  {
+    path: 'ebook/operadores',
+    loadComponent: () => import('./pages/operadores/operadores').then((c) => c.Operadores),
+  },
+  {
+    path: 'ebook/estrututa-de-controles',
+    loadComponent: () =>
+      import('./pages/estrutura-de-controles/estrutura-de-controles').then(
+        (ana) => ana.EstruturaDeControles,
+      ),
+  },
 
-       
-
-   
-
-
-    /**ultima rota */
-    {path: "**", loadComponent: () => import('./pages/welcome/welcome').then(c => c.Welcome)},
+  /**ultima rota */
+  { path: '**', loadComponent: () => import('./pages/welcome/welcome').then((c) => c.Welcome) },
 ];
--- tarefa02/src/app/pages/tipo-de-dados/tipo-de-dados.ts
+++ tarefa02/src/app/pages/tipo-de-dados/tipo-de-dados.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-tipo-de-dados',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
   templateUrl: './tipo-de-dados.html',
   styleUrl: './tipo-de-dados.css',
 })
-export class TipoDeDados {}
+export class TipoDeDados implements OnInit{
+
+  localString: string = "meu texto"; /// tipo string /// tipo char
+  localNumber: number = 100; /// tipo de número /// long, biglong, int, inteiro....
+  localBolean: boolean = false; /// tipo bollean
+  localArray: string [] = ["Leo", "Evaldina", "Rafael", "Paulo", "Monica", "Samara"];
+  localArray2: number [] = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9,);
+  localNull: null = null;
+  localNull2: null | undefined;
+  localUndefined: undefined = undefined;
+  LocalUndefine2?: undefined;
+
+  /**tipo de dados assíncronos */
+  localPromise: Promise<String> = new Promise<string>(e => {return "Aula de assíncronos"});
+  localObservable$ = new Observable(a => a.next("Aula de assíncronos Observables"));
+  localObservable2$: Observable<string> = of("Aula de assíncronos Observables2");
+
+
+  /**construtor di JS */
+  constructor () {
+
+
+
+
+  }
+  ngOnInit(): void {
+    
+  }
+
+}
